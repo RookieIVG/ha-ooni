ebb95d3e470ba090988a5334a6d422d79d11b9b2
RookieIVG <93792760+RookieIVG@users.noreply.github.com>
2026-01-08 08:08:56 +0100

    Create __init__.py

---
+++ custom_components/__init__.py
@@ -0,0 +1,18 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN, PLATFORMS
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    from .coordinator import OoniCoordinator
+
+    coordinator = OoniCoordinator(
+        hass,
+        entry.data["mac"],
+    )
+
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
